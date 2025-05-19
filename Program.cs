4d9593c98bb468b3ffd97554dad7718f3e43b6e1
akesh0805 <vchd4.texnolog@gmail.com>
2025-05-19 10:11:56 +0500

    base url / ubral

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://vchdpermissionapi-production.up.railway.app/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://vchdpermissionapi-production.up.railway.app") });
 builder.Services.AddMudServices();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
